35691fb2bf77e1b9cdbfc63f4545e40c4ac41aae
tehArchitecht <teharchitecht@gmail.com>
2020-04-03 11:33:47 +0400

    added constraint violation logging

--- src/main/java/com/github/tehArchitecht/jdbcbankingapp/logic/manager/ValidatingManager.java
+++ src/main/java/com/github/tehArchitecht/jdbcbankingapp/logic/manager/ValidatingManager.java
@@ -1,9 +1,15 @@
 package com.github.tehArchitecht.jdbcbankingapp.logic.manager;
 
+import org.apache.log4j.Logger;
+
+import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
+import java.util.Set;
 
 public class ValidatingManager {
+    private final static Logger logger = Logger.getLogger(ValidatingManager.class);
+
     private final Validator validator;
 
     protected ValidatingManager() {
@@ -11,6 +17,9 @@ public class ValidatingManager {
     }
 
     protected <T> boolean hasConstraintViolations(T object) {
-        return !validator.validate(object).isEmpty();
+        Set<ConstraintViolation<T>> violations = validator.validate(object);
+        for (ConstraintViolation<T> violation : violations)
+            logger.warn(violation.getPropertyPath() + " " + violation.getMessage());
+        return !violations.isEmpty();
     }
 }
